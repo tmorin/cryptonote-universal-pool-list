7b37962031f080bded13d259c98294d0d90390b0
tmorin <thibault@morin.io>
2017-12-11 13:37:56 +0100

    increase HTTP timeout to 3 seconds

--- src/back/config.js
+++ src/back/config.js
@@ -1,4 +1,4 @@
-export const HTTP_TIMEOUT = 1000;
+export const HTTP_TIMEOUT = 1000 * 3;
 export const CURRENCY = process.env.CURRENCY;
 if (!CURRENCY) {
     throw new Error('No currency provided as environment variable!');
