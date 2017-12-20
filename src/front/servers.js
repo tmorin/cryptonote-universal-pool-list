d3b3c92d6a91821c7aab0286f592e65d09ec95e5
tmorin <thibault@morin.io>
2017-12-20 17:28:26 +0100

    add sortable table

--- src/front/servers.js
+++ src/front/servers.js
@@ -2,47 +2,38 @@ import {floatToString, getReadableCoins, getReadableHashRateString, shorten} fro
 import $ from "jquery";
 import moment from 'moment/moment';
 import parseUrl from 'url-parse';
+import 'tablesorter';
 
 export function fetchServers() {
     const $table = $('#serverTable').addClass('loading');
     const $tbody = $table.find('tbody');
+    const $tfoot = $table.find('tfoot').html('');
     const $serverNameSelects = $('#serverNameUpdate, #serverNameRemove').html('');
 
     return fetch('./api/servers')
         .then(resp => resp.json())
         .then(payload => {
 
-            const {servers = [], updatedOn} = payload;
+                const {servers = [], updatedOn} = payload;
 
-            $('#poolsUpdatedOn').text(`updated ${moment(updatedOn).fromNow()}`);
+                $('#poolsUpdatedOn').text(`updated ${moment(updatedOn).fromNow()}`);
 
-            const serversAsHtml = servers.map(server => {
-                const front = parseUrl(server.front).protocol === 'https:';
-                const back = parseUrl(server.back).protocol === 'https:';
-                return Object.assign(server, {secured: {front, back}});
-            }).sort((s1, s2) => {
-                if (s1.error && !s2.error) {
-                    return 1;
-                } else if (!s1.error && s2.error) {
-                    return -1;
-                } else if (s1.error && s2.error) {
-                    return 0;
-                } else {
-                    return s2.stats.pool.hashrate - s1.stats.pool.hashrate;
-                }
-            }).map(server => {
-                const secLvl = server.secured.front + server.secured.back;
-                const secCls = secLvl > 1 ? 'table-success' : secLvl < 1 ? 'table-warning' : 'table-warning';
-                const secTl = `front: ${server.secured.front ? 'https' : 'http'}<br>back: ${server.secured.back ? 'https' : 'http'}`;
-                if (server.error) {
-                    return `
-                        <tr data-name="${ server.key || 'unknown' }">
-                        <td><i class="fa fa-fw fa-thumbs-o-down"></i></td>
-                        <td class="table-danger">${ server.key || 'unknown' }</td>
-                        <td colspan="9" class="table-danger">Unable to fetch data!</td>
-                        </tr>
-                    `;
-                } else {
+                const offServers = [];
+
+                const serversAsHtml = servers.map(server => {
+                    const front = parseUrl(server.front).protocol === 'https:';
+                    const back = parseUrl(server.back).protocol === 'https:';
+                    return Object.assign(server, {secured: {front, back}});
+                }).filter(server => {
+                    if (server.error) {
+                        offServers.push(server);
+                        return false;
+                    }
+                    return true;
+                }).map(server => {
+                    const secLvl = server.secured.front + server.secured.back;
+                    const secCls = secLvl > 1 ? 'table-success' : secLvl < 1 ? 'table-warning' : 'table-warning';
+                    const secTl = `front: ${server.secured.front ? 'https' : 'http'}<br>back: ${server.secured.back ? 'https' : 'http'}`;
                     const loc = server.location || 'unknown';
                     const locCell = shorten(loc);
                     const locTooltip = loc !== locCell ? loc : null;
@@ -51,9 +42,9 @@ export function fetchServers() {
                         <td class="text-center"><i class="fa fa-fw fa-check"></i></td>
                         <td data-toggle="tooltip" data-html="true" title="${secTl}" class="${secCls}"><a target="_blank" href="${server.front}">${server.name}</a></td>
                         <td data-toggle="${locTooltip && 'tooltip'}" title="${locTooltip}">${ shorten(server.location) || 'unknown' }</td>
-                        <td class="text-right">${ floatToString(server.stats.config.fee) || 0 }%</td>
+                        <td data-value="${server.stats.config.fee}" class="text-right">${ floatToString(server.stats.config.fee) || 0 }%</td>
                         <td class="text-right">${ getReadableCoins(server.stats, server.stats.config.minPaymentThreshold, 3, true) }</td>
-                        <td class="text-right">${ getReadableHashRateString(server.stats.pool.hashrate) }</td>
+                        <td data-value="${server.stats.pool.hashrate}" class="text-right">${ getReadableHashRateString(server.stats.pool.hashrate) }</td>
                         <td class="text-right">${ server.stats.pool.totalBlocks || '0' }</td>
                         <td class="text-right">${ moment(parseInt(server.stats.pool.lastBlockFound)).fromNow() }</td>
                         <td class="text-right">${ server.stats.pool.miners }</td>
@@ -61,17 +52,35 @@ export function fetchServers() {
                         <td class="text-right">${ server.stats.pool.totalPayments }</td>
                         </tr>
                     `;
-                }
-            }).join('');
+                }).join('');
 
-            $tbody.html(
-                serversAsHtml || `<tr><td colspan="11">Noting to display</td></tr>`
-            ).find('[data-toggle="tooltip"]').tooltip();
+                $tbody.html(
+                    serversAsHtml || `<tr><td colspan="11">Noting to display</td></tr>`
+                ).find('[data-toggle="tooltip"]').tooltip();
 
-            $serverNameSelects.html(
-                $tbody.find('tr').toArray().map(e => `<option>${e.dataset.name}</option>`).join('\n')
-            );
-        }).catch(err => console.error(err)).then(() => $table.removeClass('loading'));
+                $tfoot.html(offServers.map(server => `
+                    <tr data-name="${ server.key || 'unknown' }">
+                    <td><i class="fa fa-fw fa-thumbs-o-down"></i></td>
+                    <td class="table-danger">${ server.key || 'unknown' }</td>
+                    <td colspan="9" class="table-danger">Unable to fetch data!</td>
+                    </tr>
+                `));
+
+                $table.tablesorter({
+                    theme: 'bootstrap',
+                    emptyTo: 'bottom',
+                    sortList: [[5, 1]],
+                    textExtraction: {
+                        3: node => node.dataset.value,
+                        5: node => node.dataset.value
+                    }
+                });
+
+                $serverNameSelects.html(
+                    servers.map(server => `<option>${server.key}</option>`).join('\n')
+                );
+            }
+        ).catch(console.error).then(() => $table.removeClass('loading'));
 }
 
 $('a[name="refreshServers"]').click(evt => {
