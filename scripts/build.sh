8fe34b8b1554750f1ac77d5f23fa0028cf1cb58d
tmorin <thibault@morin.io>
2018-01-22 11:22:39 +0100

    #48 wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,6 @@ echo "IMAGE_NAME: $IMAGE_NAME"
 echo "building $IMAGE_NAME"
 npm install
 npm run build
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker build --rm -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
