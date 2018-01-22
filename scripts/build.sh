3e6ed47d592fa5d9a5c5357f77296d6f2358b726
tmorin <thibault@morin.io>
2018-01-22 10:44:26 +0100

    #48 wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,38 +1,13 @@
 #!/usr/bin/env sh
 
-CURRENCY=$1
-GITHUB_CLIENT_ID=$2
-GITHUB_CLIENT_SECRET=$3
-CONTAINER_NAME="$CURRENCY-universal-pool-list"
-VIRTUAL_HOST="$CURRENCY-pools.morin.io,$CURRENCY-pools.containers"
-VIRTUAL_PORT=8888
-LETSENCRYPT_HOST="$CURRENCY-pools.morin.io"
-LETSENCRYPT_EMAIL="$CURRENCY-pools@morin.io"
-BASE_URL="https://$LETSENCRYPT_HOST"
+PKG_VERSION=`grep "\"version\" *: *\"[0-9]*\.[0-9]*\.[0-9]*\" *," package.json | sed "s/\"version\" *: *\"//i;s/\" *,//i;s/ //g;"`
+echo "PKG_VERSION: $PKG_VERSION"
 
-echo "deploying $CONTAINER_NAME"
-echo "CURRENCY: $CURRENCY"
-echo "GITHUB_CLIENT_ID: $GITHUB_CLIENT_ID"
-echo "VIRTUAL_HOST: $VIRTUAL_HOST"
-echo "VIRTUAL_PORT: $VIRTUAL_PORT"
-echo "LETSENCRYPT_HOST: $LETSENCRYPT_HOST"
-echo "LETSENCRYPT_EMAIL: $LETSENCRYPT_EMAIL"
-echo "CONTAINER_NAME: $CONTAINER_NAME"
+IMAGE_NAME="thibaultmorin/cryptonote-universal-pool-list:$PKG_VERSION"
+echo "IMAGE_NAME: $IMAGE_NAME"
 
-docker kill ${CONTAINER_NAME}
-docker rm ${CONTAINER_NAME}
-
-docker build --rm -t ${CONTAINER_NAME} .
-docker rm -f ${CONTAINER_NAME}
-docker run --name ${CONTAINER_NAME} \
-    -e CURRENCY=${CURRENCY} \
-    -e ENV_NAME=${CONTAINER_NAME} \
-    -e VIRTUAL_HOST=${VIRTUAL_HOST} \
-    -e VIRTUAL_PORT=8888 \
-    -e LETSENCRYPT_HOST=${LETSENCRYPT_HOST} \
-    -e LETSENCRYPT_EMAIL=${LETSENCRYPT_EMAIL} \
-    -e GITHUB_CLIENT_ID=${GITHUB_CLIENT_ID} \
-    -e GITHUB_CLIENT_SECRET=${GITHUB_CLIENT_SECRET} \
-    -e BASE_URL=${BASE_URL} \
-    -p 8888:8888 \
-    ${CONTAINER_NAME}
+echo "building $IMAGE_NAME"
+npm install
+npm run build
+docker build --rm -t ${IMAGE_NAME} .
+docker push ${IMAGE_NAME}
