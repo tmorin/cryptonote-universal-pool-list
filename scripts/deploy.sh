ccebf374edfbece708e3a53c10523bfe27f96d36
tibo <thibault@morin.io>
2018-05-23 21:36:37 +0200

    fix: change email for let's encrypt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ CONTAINER_NAME="$CURRENCY-universal-pool-list"
 VIRTUAL_HOST="$CURRENCY-pools.morin.io,$CURRENCY-pools.containers"
 VIRTUAL_PORT=8888
 LETSENCRYPT_HOST="$CURRENCY-pools.morin.io"
-LETSENCRYPT_EMAIL="$CURRENCY-pools@morin.io"
+LETSENCRYPT_EMAIL="pools@morin.io"
 BASE_URL="https://$LETSENCRYPT_HOST"
 
 echo "deploying $CONTAINER_NAME"
